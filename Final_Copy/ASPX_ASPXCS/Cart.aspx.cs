41e351d63d90af3c90822df1d456cd43c8704d8b
Eric Lorentzen <doctor.c.eze@gmail.com>
2016-07-17 22:45:50 -0500

    Update Cart.aspx.cs

--- Final_Copy/ASPX_ASPXCS/Cart.aspx.cs
+++ Final_Copy/ASPX_ASPXCS/Cart.aspx.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-
 public partial class ASPX_ASPXCS_Cart : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
@@ -13,7 +12,6 @@ public partial class ASPX_ASPXCS_Cart : System.Web.UI.Page
             try
             {
                 ShoppingCart newCart = (ShoppingCart)Session["cart"];
-
                 this.shopCartGrid.DataSource = newCart.Basket;
                 this.shopCartGrid.DataBind();
 
@@ -22,7 +20,6 @@ public partial class ASPX_ASPXCS_Cart : System.Web.UI.Page
                 Total_lbl.Text = string.Format("{0:C}", newCart.Total);
             }
             catch (Exception err) { }
-        
     }
 
     protected void shopCartGrid_SelectedIndexChanged(object sender, EventArgs e)
@@ -38,19 +35,23 @@ public partial class ASPX_ASPXCS_Cart : System.Web.UI.Page
 
     protected void shopCartGrid_RowDeleting(object sender, GridViewDeleteEventArgs e)
     {
-        try { 
-        ShoppingCart temp = (ShoppingCart)Session["cart"];
-            var key = (int)shopCartGrid.DataKeys[e.RowIndex].Value;
-        temp.removeItem(key);
-            Session["cart"] = temp;
-        shopCartGrid.DataSource = temp.Basket; 
-        shopCartGrid.DataBind();
-        Response.Redirect("cart.aspx");
-
-        //SubTotal_lbl.Text = string.Format("{0:C}", temp.SubTotal);
-        //Tax_lbl.Text = string.Format("{0:C}", temp.Tax);
-        //Total_lbl.Text = string.Format("{0:C}", temp.Total);
-    }
-        catch (Exception err) { Response.Write(err.Message + " " + err.StackTrace); }
+        try 
+        { 
+            ShoppingCart temp = (ShoppingCart)Session["cart"];
+                var key = (int)shopCartGrid.DataKeys[e.RowIndex].Value;
+            temp.removeItem(key);
+                Session["cart"] = temp;
+            shopCartGrid.DataSource = temp.Basket; 
+            shopCartGrid.DataBind();
+            Response.Redirect("cart.aspx");
+    
+            //SubTotal_lbl.Text = string.Format("{0:C}", temp.SubTotal);
+            //Tax_lbl.Text = string.Format("{0:C}", temp.Tax);
+            //Total_lbl.Text = string.Format("{0:C}", temp.Total);
+        }
+        catch (Exception err) 
+        { 
+            //Response.Write(err.Message + " " + err.StackTrace); 
+        }
     }
 }
